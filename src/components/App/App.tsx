757a667022756776b5ac524083c298fc3220925a
konstantin <gktin@yandex.ru>
2026-05-09 09:06:47 +0700

    fix images import

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import bun from '@/assets/logo.svg';
+import react from '@/assets/react.svg';
 import type React from 'react';
 
 import css from './styles.module.css';
@@ -6,8 +8,8 @@ export const App: React.FC = () => {
 	return (
 		<div className={css.app}>
 			<div className={css.logo_container}>
-				<img alt="Bun Logo" className={`${css.logo} ${css.bun_logo}`} src="/logo.svg" />
-				<img alt="React Logo" className={`${css.logo} ${css.react_logo}`} src="/react.svg" />
+				<img alt="Bun Logo" className={`${css.logo} ${css.bun_logo}`} src={bun} />
+				<img alt="React Logo" className={`${css.logo} ${css.react_logo}`} src={react}/>
 			</div>
 
 			<h1>Bun + React</h1>
